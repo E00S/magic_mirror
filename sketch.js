0326c8ab69e909fa349c389ac444d9a668783e6e
E00S <leonrogge@lclark.edu>
2019-11-14 02:37:55 -0800

    a

--- sketch.js
+++ sketch.js
@@ -1,9 +1,26 @@
 let capture
 let tracker
 
+// --- 
+let xspacing = 16; // Distance between each horizontal location
+let w; // Width of entire wave
+let theta = 180.0; // Start angle at 0
+let amplitude = 30.0; // Height of wave
+let period = 200.0; // How many pixels before the wave repeats
+let dx; // Value for incrementing x
+let yvalues; // Using an array to store height values for the wave
+let old_x = 0;
+let old_y = 0;
+let X = 0;
+let Y = 0;
+let start_x;
+let start_y;
+let a = 0;
+//---  
+
 function setup() {
 
-    createCanvas(800, 600)
+    createCanvas(800, 800)
 
     // start capturing video
     capture = createCapture(VIDEO)
@@ -15,6 +32,44 @@ function setup() {
     tracker.init()
     tracker.start(capture.elt)    
 
+    // -- 
+    w = width + 16;
+    dx = (TWO_PI / period) * xspacing;
+    yvalues = new Array(floor(w / xspacing));
+    start_x = height / 2;
+    start_y = 200;
+
+}
+
+function calcWave() {
+    // Increment theta (try different values for
+    // 'angular velocity' here)
+    theta -= 0.08;
+
+    // For every x value, calculate a y value with sine function
+    let x = theta;
+    for (let i = 0; i < yvalues.length; i++) {
+        yvalues[i] = sin(x) * amplitude;
+        x += dx;
+    }
+}
+
+function renderWave(a, color) {
+    push();
+    stroke(color);
+    strokeWeight(30);
+    old_x = start_y + 0 * xspacing;
+    old_y = start_x + a + yvalues[0];
+    // A simple way to draw the wave with an ellipse at each location
+    for (let x = 0; x < yvalues.length; x++) {
+        // ellipse(x * xspacing, height / 2 + yvalues[x], 16, 16);
+        Y = start_x + a + yvalues[x];
+        X = start_y + x * xspacing;
+        line(old_y, old_x, Y, X);
+        old_x = X;
+        old_y = Y;
+    }
+    pop();
 }
 
 
@@ -43,41 +98,32 @@ function draw() {
 
             i += 1
         }
-
-        // overlay eyes
-        let leftEyeX = positions[32][0]
-        let leftEyeY = positions[32][1]
-
-        let rightEyeX = positions[27][0]
-        let rightEyeY = positions[27][1]
-        
-        push()
-        fill(255, 0, 0)
-        ellipse(leftEyeX, leftEyeY, 20, 20)
-        ellipse(rightEyeX, rightEyeY, 20, 20)
-        pop()        
-
-        // measure distances between features
-        let noseX = positions[62][0]
-        let noseY = positions[62][1]
-
-        let leftNostrilX = positions[43][0]
-        let leftNostrilY = positions[43][1]
-
-        let rightNostrilX = positions[42][0]
-        let rightNostrilY = positions[42][1]
-
-        let distanceLeft = dist(noseX, noseY, leftNostrilX, leftNostrilY) 
-        let distanceRight = dist(noseX, noseY, rightNostrilX, rightNostrilY)
-
-        if (distanceLeft > distanceRight) {
-            print('facing right')
-        } else {
-            print('facing left')
-        }
-
-
     }
+    // let noseX = positions[62][0]
+    // let l_lip_x = positions[44][0];
+    // let l_lip_y = positions[44][1];
 
+    // let r_lip_x = positions[50][0] - 5;
+    // let r_lip_y = positions[50][1];
+
+    // let u_lip_x = positions[60][0];
+    // let u_lip_y = positions[60][1];
+
+    // let d_lip_x = positions[57][0];
+    // let d_lip_y = positions[57][1];
+    // //l m 44
+    // //r m 50 - 5
+    // //u t 60
+    // //d 57 
+    // if(dist(u_lip_x, u_lip_y, d_lip_x, d_lip_y) > 20) {
+    //     calcWave();
+    //     renderWave(0, 'red'); //red
+    //     renderWave(30, 'orange'); //orange
+    //     renderWave(60, 'yellow'); //yellow
+    //     renderWave(90, 'green'); //green
+    //     renderWave(120, 'lightblue'); //light_blue
+    //     renderWave(150, 'blue'); //blue
+    //     renderWave(180, 'purple'); //purple
+    // }  
 }
 
